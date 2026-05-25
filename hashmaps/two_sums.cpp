19c4c0eca40b78005b97c5ef241e5c7f5fb39f5a
sanvi 𖦹 <singhsanvi303@gmail.com>
2026-05-25 23:26:20 +0530

    Add files via upload

---
+++ hashmaps/two_sums.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mpp;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int remain = target - nums[i];
+            if (mpp.find(remain) != mpp.end())
+            {
+                return {i, mpp[remain]};
+            }
+            mpp[nums[i]] = i;
+        }
+        return {};
+    }
+};
